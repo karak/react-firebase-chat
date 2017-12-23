bd4b1e9af5371657591c73666adfbe268fd64cd4
karak <karak97@gmail.com>
2017-12-23 17:37:11 +0900

    Removed type definition file of @storybook/react/demo

--- stories/index.tsx
+++ stories/index.tsx
@@ -1,19 +1,9 @@
-/// <reference path="./@my-types/@storybook__react__demo.d.ts"/>
 import * as React from 'react';
 
 import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
-import { linkTo } from '@storybook/addon-links';
-import { Button, Welcome } from '@storybook/react/demo';
 import Message from '../src/components/Message';
 import MessageBoard from '../src/components/MessageBoard';
 import 'semantic-ui-css/semantic.min.css';
-
-storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
-
-storiesOf('Button', module)
-  .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
-  .add('with some emoji', () => <Button onClick={action('clicked')}>😀 😎 👍 💯</Button>);
 
 storiesOf('Message', module)
   .add('default', () => (
